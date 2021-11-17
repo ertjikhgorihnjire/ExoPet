7a81a2fb70491f58458491d87ad4f2bb018f9d33
Gianluca Onisanti <94574861+ertjikhgorihnjire@users.noreply.github.com>
2021-11-17 20:25:31 -0300

    Update Programa.java

--- ExoPet/src/P/Programa.java
+++ ExoPet/src/P/Programa.java
@@ -7,13 +7,13 @@ public class Programa {
 	
 	public static void main(String[] args) {
 		
-		Scanner sc = new Scanner(System.in);	/// Vari·veis
-			String nome = " ";
-				int idade = 0;
-					boolean check = true;
-						int dinheiro = 0;
-							ArrayList<Animal> animais = new ArrayList<Animal>();
-								Dono dono;
+		Scanner sc = new Scanner(System.in);	/// Vari√°veis
+		String nome = " ";
+		int idade = 0;
+		oolean check = true;
+		int dinheiro = 0;
+		ArrayList<Animal> animais = new ArrayList<Animal>();
+		Dono dono;
 		
 		System.out.print("Digite seu nome: ");
 		nome = sc.nextLine();
@@ -21,14 +21,14 @@ public class Programa {
 		System.out.print("Digite sua idade: ");
 		idade = sc.nextInt();
 		
-		System.out.print("Digite seu saldo banc·rio: ");
+		System.out.print("Digite seu saldo banc√°rio: ");
 		dinheiro = sc.nextInt();
 		
 		
-		dono = new Dono(nome, idade, check, dinheiro, animais);		/// InstanciaÁ„o
+		dono = new Dono(nome, idade, check, dinheiro, animais);		/// Instancia√ß√£o
 		System.out.println("\n" + "Nome: " + dono.nome);
 		System.out.println("Idade: " + dono.idade);
-		System.out.println("LicenÁa: " + dono.check);			/// Status passagem de dados = ok
+		System.out.println("Licen√ßa: " + dono.check);			/// Status passagem de dados = ok
 		System.out.println("Dinheiro: " + dono.dinheiro);
 		
 		sc.close();
